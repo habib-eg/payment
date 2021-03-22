8a373d86dede6876432e568f50d71d364d01be21
mohamedhabibwork <mohamedhabibwork@gmail.com>
2021-03-22 21:21:59 +0200

    push cart setting

--- src/Helpers/CartDataBase.php
+++ src/Helpers/CartDataBase.php
@@ -5,8 +5,8 @@ namespace Habib\Payment\Helpers;
 use Exception;
 use Habib\Payment\CartInterface;
 use Habib\Payment\Models\Cart;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class CartDataBase implements CartInterface
 {
@@ -22,38 +22,37 @@ class CartDataBase implements CartInterface
     }
 
     /**
-     * @param $validated
-     * @return array|Cart|Collection
+     * @return $this
      */
-    public function add($validated)
+    public function addCartToLoginUser(): self
     {
-        return $this->user->carts()->firstOrCreate(
-            collect($validated)->only(['cartable_id','cartable_type'])->toArray(),
-            collect($validated)->except(['cartable','cartable_id','cartable_type'])->toArray()
-        ) ;
+        if (!$this->user) {
+            return $this;
+        }
+
+        $this->removeCache();
+        $cartSession = new CartSession();
+        $cart = $cartSession->getCarts();
+
+        if ($cart->count() > 0) {
+            $cart->map(function ($cart) {
+                $this->user->carts()->firstOrCreate(
+                    collect($cart)->only(['cartable_id', 'cartable_type'])->toArray(),
+                    collect($cart)->except(['cartable', 'cartable_id', 'cartable_type'])->toArray()
+                );
+            });
+
+            $cartSession->setCart([]);
+        }
+        return $this;
     }
 
-    /**
-     * @return array|Cart[]|Collection
-     */
-    public function all()
+    public function removeCache()
     {
-        return cache()->remember('carts.'.$this->user->getKey(),now()->addSeconds(10),fn()=>$this->user->carts()->with(['cartable'])->get());
-    }
-
-    /**
-     * @param array $validated
-     * @return array|Cart|null
-     */
-    public function findInCart(array $validated)
-    {
-        return $this->user->carts()->loader()
-        ->when($validated['id'] ?? null, fn ($q) => $q->where('id', $validated['id']))
-        ->when($validated['cartable_type'] ?? null, fn ($q) => $q->where('cartable_type', $validated['cartable_type']))
-        ->when($validated['cartable_id'] ?? null, fn ($q) => $q->where('cartable_id', $validated['cartable_id']))
-        ->when($this->user ?? null, fn ($q) => $q->where('owner_id', $this->user->id))
-        ->where('owner_type',$this->user->getMorphClass())
-        ->first();
+        $key = 'carts.' . $this->user->getKey();
+        if (cache()->has($key)) {
+            cache()->forget($key);
+        }
     }
 
     /**
@@ -71,8 +70,24 @@ class CartDataBase implements CartInterface
                 $cart['options'][$key] = $option;
             }
             $cart->save();
+            $this->removeCache();
         }
         return $cart;
+    }
+
+    /**
+     * @param array $validated
+     * @return array|Cart|null
+     */
+    public function findInCart(array $validated)
+    {
+        return $this->user->carts()->loader()
+            ->when($validated['id'] ?? null, fn($q) => $q->where('id', $validated['id']))
+            ->when($validated['cartable_type'] ?? null, fn($q) => $q->where('cartable_type', $validated['cartable_type']))
+            ->when($validated['cartable_id'] ?? null, fn($q) => $q->where('cartable_id', $validated['cartable_id']))
+            ->when($this->user ?? null, fn($q) => $q->where('owner_id', $this->user->id))
+            ->where('owner_type', $this->user->getMorphClass())
+            ->first();
     }
 
     /**
@@ -83,11 +98,20 @@ class CartDataBase implements CartInterface
     {
         try {
             $this->findInCart($validated)->delete();
+            $this->removeCache();
             return $this->all();
         } catch (Exception $e) {
 
         }
         return false;
+    }
+
+    /**
+     * @return array|Cart[]|Collection
+     */
+    public function all()
+    {
+        return cache()->remember('carts.' . $this->user->getKey(), now()->addSeconds(3), fn() => $this->user->carts()->with(['cartable'])->get());
     }
 
     /**
@@ -99,33 +123,20 @@ class CartDataBase implements CartInterface
     public function addModelToCart(Model $model, int $qty = 1, array $options = [])
     {
         $validated = ["cartable_id" => $model->id, "cartable_type" => $model->getMorphClass(), "qty" => ($qty >= 1 ? $qty : 1), 'options' => $options,];
-        return  $this->add($validated);
+        return $this->add($validated);
     }
-
 
     /**
-     * @return $this
+     * @param $validated
+     * @return array|Cart|Collection
      */
-    public function addCartToLoginUser(): self
+    public function add($validated)
     {
-        if (!$this->user) {
-            return $this;
-        }
-        $cartSession = new CartSession();
-        $cart = $cartSession->getCarts();
-
-        if ($cart->count() > 0) {
-                $cart->map(function ($cart) {
-                    $this->user->carts()->firstOrCreate(
-                        collect($cart)->only(['cartable_id','cartable_type'])->toArray(),
-                        collect($cart)->except(['cartable','cartable_id','cartable_type'])->toArray()
-                    ) ;
-                });
-
-                $cartSession->setCart([]);
-            }
-
-        return $this;
+        $cart = $this->user->carts()->firstOrCreate(
+            collect($validated)->only(['cartable_id', 'cartable_type'])->toArray(),
+            collect($validated)->except(['cartable', 'cartable_id', 'cartable_type'])->toArray()
+        );
+        $this->removeCache();
+        return $cart;
     }
-
 }
